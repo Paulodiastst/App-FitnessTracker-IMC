f7db98a1f9336c8e28833c503419550acf59adcf
Paulo Roberto <79487813+Paulodiastst@users.noreply.github.com>
2022-05-22 18:59:16 -0300

    Intenções e Activities

--- app/src/main/java/co/tiagoaguiar/codelab/myapplication/MainActivity.java
+++ app/src/main/java/co/tiagoaguiar/codelab/myapplication/MainActivity.java
@@ -1,14 +1,29 @@
 package co.tiagoaguiar.codelab.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+	private View btnImc;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		btnImc = findViewById(R.id.btn_imc);
+
+		btnImc.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Intent intent = new Intent(MainActivity.this, ImcActivity.class);
+				startActivity(intent);
+			}
+		});
+
 	}
 }
