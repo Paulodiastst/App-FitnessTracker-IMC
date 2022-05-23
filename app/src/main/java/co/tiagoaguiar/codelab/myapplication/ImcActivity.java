0ba1fb4faf8a9237a493b04777ceae40b76c32e4
Paulo Roberto <79487813+Paulodiastst@users.noreply.github.com>
2022-05-22 23:41:15 -0300

    Validação de Formulários

--- app/src/main/java/co/tiagoaguiar/codelab/myapplication/ImcActivity.java
+++ app/src/main/java/co/tiagoaguiar/codelab/myapplication/ImcActivity.java
@@ -1,14 +1,87 @@
 package co.tiagoaguiar.codelab.myapplication;
 
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.number.IntegerWidth;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class ImcActivity extends AppCompatActivity {
+
+    private EditText editHeight;
+    private EditText editWeight;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_imc);
+
+        editHeight = findViewById(R.id.edit_imc_height);
+        editWeight = findViewById(R.id.edit_imc_weight);
+
+        Button btnSend = findViewById(R.id.btn_imc_send);
+
+        btnSend.setOnClickListener(new View.OnClickListener() { //Bloco de código de execução método OnClick
+            @Override
+            public void onClick(View view) {
+                if (!validate()) {
+                    Toast.makeText(ImcActivity.this, R.string.fields_messages, Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                String sHeight = editHeight.getText().toString();
+                String sWeight = editWeight.getText().toString();
+
+                int height = Integer.parseInt(sHeight);
+                int weight = Integer.parseInt(sWeight);
+
+                double result = calculateImc(height, weight);
+                Log.d("TESTE", "resultado:" + result);
+
+                int imcResponseId = imcResponse(result);
+
+                Toast.makeText(ImcActivity.this, imcResponseId, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
+
+    @StringRes
+    private int imcResponse(double imc) {
+        if (imc < 15)
+            return R.string.imc_severely_low_weight;
+        else if (imc < 16)
+            return R.string.imc_very_low_weight;
+        else if (imc < 18.5)
+            return R.string.imc_low_weight;
+        else if (imc < 25)
+            return R.string.normal;
+        else if (imc < 30)
+            return R.string.imc_severely_high_weight;
+        else if (imc < 35)
+            return R.string.imc_so_high_weight;
+        else if (imc < 40)
+            return R.string.imc_severely_high_weight;
+        else
+            return R.string.imc_extreme_weight;
+        }
+
+    private double calculateImc(int height, int weight) {
+        // peso / (altura * altura)
+        return weight / ( ((double) height / 100) * ((double) height / 100) );
+    }
+
+                // Para que seja valido não deve comerçar com 0 e não deve ser vazio.
+    private boolean validate() {
+        return  (!editHeight.getText().toString().startsWith("0")
+                && !editWeight.getText().toString().startsWith("0")
+                && !editHeight.getText().toString().isEmpty()
+                && !editWeight.getText().toString().isEmpty());
+    }
+
 }
